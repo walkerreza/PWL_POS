fc3312fea2f7b6c790ad25d1ba0c975560c942c9
anka reza <rezawalker02@gmail.com>
2026-03-11 22:34:14 +0700

    praktikum 2.4

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -38,18 +38,118 @@ use Illuminate\Support\Facades\Hash;
 //     return view('user', ['data' => $user]);
 // }
 
+// public function index()
+// {
+//     $data = [
+//         'level_id'  => 2,
+//         'username'  => 'manager_tiga',
+//         'nama'      => 'Manager 3',
+//         'password'  => Hash::make('12345')
+//     ];
+//     UserModel::create($data);
+
+//     $user = UserModel::all();
+//     return view('user', ['data' => $user]);
+// }
+// 
+//  public function index()
+// {
+//     $user = UserModel::find(1);
+//     return view('user', ['data' => $user]);
+// }
+
+// public function index()
+// {
+//     $user = UserModel::where('level_id', 1)->first();
+//     return view('user', ['data' => $user]);
+// }
+
+// public function index()
+// {
+//     $user = UserModel::firstWhere('level_id', 1);
+//     return view('user', ['data' => $user]);
+// }
+// public function index()
+// {
+//     $user = UserModel::findOr(1, ['username', 'nama'], function () {
+//         abort(404);
+//     });
+
+//     return view('user', ['data' => $user]);
+// }
+// public function index()
+// {
+//     $user = UserModel::findOr(20, ['username', 'nama'], function () {
+//         abort(404);
+//     });
+
+//     return view('user', ['data' => $user]);
+// } 
+ 
+
+// public function index()
+// {
+//     $user = UserModel::findOrFail(1);
+//     return view('user', ['data' => $user]);
+// }
+
+// public function index()
+// {
+//     $user = UserModel::where('username', 'manager9')->firstOrFail();
+//     return view('user', ['data' => $user]);
+// }
+
+   
+//  public function index()
+// {
+//     $user = UserModel::where('level_id', 2)->count();
+//      //dd($user);
+//     return view('user', ['data' => $user]);
+// }
+
+// public function index()
+// {
+//     $user = UserModel::firstOrCreate([
+//         'username' => 'manager',
+//         'nama'     => 'Manager',
+//     ]);
+
+//     return view('user', ['data' => $user]);
+// }
+// public function index()
+// {
+//     $user = UserModel::firstOrCreate(
+//         [
+//             'username' => 'manager22',
+//             'nama'     => 'Manager Dua Dua',
+//             'password' => Hash::make('12345'),
+//             'level_id' => 2
+//         ]
+//     );
+//     return view('user', ['data' => $user]);
+// }
+// public function index()
+// {
+//     $user = UserModel::firstOrNew(
+//         [
+//             'username' => 'manager',
+//             'nama'     => 'Manager',
+//         ]
+//     );
+//     return view('user', ['data' => $user]);
+// }
 public function index()
 {
-    $data = [
-        'level_id'  => 2,
-        'username'  => 'manager_tiga',
-        'nama'      => 'Manager 3',
-        'password'  => Hash::make('12345')
-    ];
-    UserModel::create($data);
-
-    $user = UserModel::all();
+    $user = UserModel::firstOrNew(
+        [
+            'username' => 'manager33',
+            'nama'     => 'Manager Tiga Tiga',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
+        ]
+    );
+     $user->save();
     return view('user', ['data' => $user]);
 }
 
- }
+}
